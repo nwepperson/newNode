201a36c33a811cc28d4dc91a8151c9145e416470
Nate Epperson <nate@mbp.attlocal.net>
2017-01-12 17:48:41 -0500

    removed users route

--- newnode.sh
+++ newnode.sh
@@ -17,7 +17,6 @@ cp ./files/package.json ../$name/
 cp ./files/error.ejs ../$name/views/
 cp ./files/index.ejs ../$name/views/
 cp ./files/index.js ../$name/routes/
-cp ./files/users.js ../$name/routes/
 
 cd ../$name/
 npm init
